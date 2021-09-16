e9c52c8a782543df4c74bc83e935912782459a9a
husainkkarachi <husainkkarachiwala@gmail.com>
2021-09-16 15:39:57 +0530

    WebApp Commit

---
+++ WebApp/src/main/java/com/lti/jenkins/MyCalculator.java
@@ -0,0 +1,12 @@
+package com.lti.jenkins;
+
+public class MyCalculator {
+
+	public int sum(int x, int y) {
+		return x+y;
+	}
+	
+	public int diff(int x, int y) {
+		return x-y;
+	}
+}
---
+++ WebApp/src/test/java/MyCalcTest.java
@@ -0,0 +1,24 @@
+import static org.junit.Assert.*;
+
+import com.lti.jenkins.MyCalculator;
+
+import org.junit.Test;
+
+public class MyCalcTest {
+
+	@Test
+	public void test1() {
+		assertEquals(5, new MyCalculator().sum(2, 3));
+	}
+	
+	@Test
+	public void test2() {
+		assertEquals(10, new MyCalculator().sum(10, -5));
+	}
+	
+	@Test
+	public void test3() {
+		assertEquals(13, new MyCalculator().sum(13, 0));
+	}
+
+}
